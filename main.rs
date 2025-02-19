af05167fa38a11e8425f3baec4df667c6b5510f5
Rahul Patle <141508051+mentei@users.noreply.github.com>
2025-02-20 00:00:24 +0530

    Add files via upload
    day2 of building a mini project in rust lang

--- main.rs
+++ main.rs
@@ -1,11 +1,3 @@
-
-use std::io;
-
-
 fn main() {
-    println!("Dear-user guss a  number! ");
-    println!("Please enter your guss: ");
-    let mut guss = String::new();
-    io::stdio().read_line(&mut guss).expect("Error reading line");
-    println!("you  gussed :{}", guss);
+    println!("Hello, world!");
 }
