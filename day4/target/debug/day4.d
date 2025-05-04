592bcc86a842267fc037808082040f65999d93fa
Rahul Patle <141508051+mentei@users.noreply.github.com>
2025-05-04 20:40:53 +0530

    looping started

---
+++ day4/target/debug/day4.d
@@ -0,0 +1,1 @@
+D:\Rust\ master\memories-of-rust\day4\target\debug\day4.exe: D:\Rust\ master\memories-of-rust\day4\src\main.rs
---
+++ day4/target/debug/deps/day4-c276214b78a1c9cf.d
@@ -0,0 +1,5 @@
+D:\Rust master\memories-of-rust\day4\target\debug\deps\day4-c276214b78a1c9cf.exe: src\main.rs
+
+D:\Rust master\memories-of-rust\day4\target\debug\deps\day4-c276214b78a1c9cf.d: src\main.rs
+
+src\main.rs:
