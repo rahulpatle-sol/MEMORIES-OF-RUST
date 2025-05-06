7af909340b204ee542807665febeff298bb8528a
Rahul Patle <141508051+mentei@users.noreply.github.com>
2025-05-06 23:48:20 +0530

    satck & heap

---
+++ day5/target/debug/day5.d
@@ -0,0 +1,1 @@
+D:\Rust\ master\memories-of-rust\day5\target\debug\day5.exe: D:\Rust\ master\memories-of-rust\day5\src\main.rs
---
+++ day5/target/debug/deps/day5-1dfc96350a7d1cdb.d
@@ -0,0 +1,5 @@
+D:\Rust master\memories-of-rust\day5\target\debug\deps\day5-1dfc96350a7d1cdb.exe: src\main.rs
+
+D:\Rust master\memories-of-rust\day5\target\debug\deps\day5-1dfc96350a7d1cdb.d: src\main.rs
+
+src\main.rs:
