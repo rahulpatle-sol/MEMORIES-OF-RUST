682d40e5c2cd9d8e2946409644167d69bbd7578d
Rahul Patle <141508051+mentei@users.noreply.github.com>
2025-06-15 06:52:08 +0530

    result enum

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+match divide(100,45){
+    Ok (result )=>{
+        println!("Result is {}",result)
+    }
+    Err(e)=>{
+        println!("Error: {}", e)
+    }
+}
+}
+
+///  Result Enum is used to handle errors in Rust.
+/// 
+fn divide(a:i32,b:i32)->Result<i32,String>{
+    if b==0{
+        Err (String::from("Cannot divide by zero"))
+    }
+
+    else{
+        Ok(a/b)
+    }
 }
